46e69d50808236ddd6294a045d0b77be1e308f7c
Shugen <shugen002@users.noreply.github.com>
2023-04-25 05:15:06 +0800

    1

--- DesktopLricsFix.cpp
+++ DesktopLricsFix.cpp
@@ -51,24 +51,13 @@ int main()
 	if (current_style & WS_EX_TOOLWINDOW) {
 		std::cout << "检测到 WS_EX_TOOLWINDOW 样式，尝试移除。" << "\n";
 		SetWindowLongPtr(window_handle, GWL_EXSTYLE, current_style ^ WS_EX_TOOLWINDOW);
-		std::cout << "已移除 WS_EX_TOOLWINDOW 样式，请在OBS中使用窗口捕获中的“Windows 10 (1903及以上版本)”方式捕获“桌面歌词”窗口" << "\n";
-		std::cout << "再次提醒，任务栏中出现一个无法关闭的“桌面歌词”属正常现象" << "\n";
+		std::cout << "已移除 WS_EX_TOOLWINDOW 样式，请在OBS中使用窗口捕获中的“Windows 10 (1903及以上版本)”方式捕获“桌面歌词”窗口\n";
+		std::cout << "再次提醒，任务栏中出现一个无法关闭的“桌面歌词”属正常现象\n";
 	}
 	else {
-		std::cout << "未检测到 WS_EX_TOOLWINDOW 样式，退出。" << "\n";
+		std::cout << "未检测到 WS_EX_TOOLWINDOW 样式，退出。\n";
 	}
 	std::cout << "按任意键退出...";
 	std::cin.get();
 	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
